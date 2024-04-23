cc9b2baeed3fc575dcbefb52477fd7ca452851f7
Saraalmogren <147044409+Saraalmogren@users.noreply.github.com>
2024-04-23 23:27:24 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
 
 	static int P1, P2, BT; //P1: length of queue 1, P2: length of queue 2, BT: sum of processes' burst time
-	static int q1counter, q2counter; //keep track of the number of processes currently in queue 1 and queue 2.
-	static PCB processes[], Q1[], Q2[]; 
+	static int q1counter, q2counter; //number of processes in each queue
+	static PCB processes[], Q1[], Q2[];
 	static LinkedList<String> ProcessesChart;
 	static Scanner input = new Scanner(System.in);
 
@@ -28,7 +28,7 @@ public class Main {
 					System.out.println("you need to enter processes first.");
 					break;
 				}
-                //PrintReport1();
+                PrintReport1();
 				break;
 			case 3:
 				System.out.println("Goodbye.");
@@ -76,74 +76,69 @@ public class Main {
         else
             P2++;
 
-        processes[i] = new PCB(String.format("P%d", i), arrivalT, burstT, priority);// create PCB object and stores them in process array
+        processes[i] = new PCB(String.format("P%d", i), arrivalT, burstT, priority);
 
     } // end for loop
-
-    // create Q1 and Q2 array based on the priority
     Q1 = new PCB[P1];
     Q2 = new PCB[P2];
-    MLQ(); // for scheduling the multilevel queue
+    MLQ();
 
     }
 
     static void MLQ() { //multilevel queue scheduler with fixed preemption
+    ProcessesChart = new LinkedList<String>();
+    int timer = 0;
+
+    while(timer < BT){
+        assign(timer);
+        if(Q1.length > 0 && Q1[0] != null){
+            if(Q2.length > 0 && Q2[0] != null && Q2[0].finishedBurst != 0)
+            reschedule();
+
+            Q1[0].setStartTime(timer);
+            Q1[0].setTerminationTime(timer + Q1[0].getBurstT());
+            Q1[0].setTurnAroundTime(Q1[0].getTerminationTime() - Q1[0].getArrivalT());
+            Q1[0].setWaitingTime(Q1[0].getTurnAroundTime() - Q1[0].getBurstT());
+            Q1[0].setResponseTime(Q1[0].getStartTime() - Q1[0].getArrivalT());
+
+            int t = timer;
+            timer += Q1[0].getBurstT();
+
+            while(t < timer)
+            ProcessesChart.add(t++, Q1[0].getPID());
+
+            shift();
+        }
+        else if(Q2[0]finishedBurst == Q2[0].getBurstT()){
+            Q2[0].setStartTime(timer);
+            Q2[0].setTurnAroundTime(Q2[0].getTerminationTime() - Q2[0].getArrivalT());
+            Q2[0].setWaitingTime(Q2[0].getTurnAroundTime() - Q2[0].getBurstT());
+            Q2[0].setResponseTime(Q2[0].getStartTime() - Q2[0].getArrivalT());
+            reschedule();
+        }
+    }
+    else{
+        ProcessesChart.add(timer++, "");//why after not before?
+        BT++;
+
+    }
     }
 
-    static void shift() {
-		for(int i=1; i<q1counter; i++) {
-			Q1[i-1] = Q1[i];
-		}
-		Q1[--q1counter] = null;
-	}
-
-    // another way for shiffting
-    /**static void shift1() {
-        // Loop from the second element to the second-last element (exclusive)
-        for (int i = 1; i < q1counter - 1; i++) {
-          // Overwrite the current element with the next element
-          Q1[i] = Q1[i + 1];
+    static void assign(int arrivalTime){
+        PCB arrivedQ[] = arrivedprocesses(arrival);//arrivedprocesses from where did we get it ?
+        int c = q2counter;
+        for(PCB process : arrivedQ){
+            if(Q1.length >0 && process.getPriority() == 1){//where did we get Priority?
+                q1[q1counter++] = process;}
+                else if(Q2.length > 0){
+                    Q2[q2counter++] = process;}
+                    process.assigned = true; //assigned?
+                }
+              SJF();// shorest job first call here
+              RR();//round robuin call here  
+            }
         }
-        
-        // Set the last element to null
-        Q1[q1counter - 1] = null;
-        
-        // Decrement the counter
-        q1counter--;
-      }**/
 
-      static void reschedule() {
-		PCB tempProcess = Q2[0];
-		for(int i=1; i<q2counter; i++) { // for shiffting
-			Q2[i-1] = Q2[i];
-		}
-        if(tempProcess.getCpuBurst() != tempProcess.finishedBurst) {
-			Q2[q2counter-1] = tempProcess;
-		}
-		else {
-			Q2[--q2counter] = null;
-		}
-	}
-
-    static PCB[] arrivedprocesses(int arrival) {
-		int i=0;
-		for(PCB process : processes) {
-			if(process.getArrivalTime()<=arrival && !process.assigned) {
-				i++;
-			}
-		}
-
-		PCB arrived[] = new PCB[i];
-		i=0;
-		for(PCB process : processes) {
-			if(process.getArrivalTime()<=arrival && !process.assigned) {
-				arrived[i++] = process;
-			}
-		}
-
-		return arrived;
-	}
     
 
 
-}
